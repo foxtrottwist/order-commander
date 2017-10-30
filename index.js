726e61e04005ec456af8d5dd657e3a8bf56a7efd
Lawrence Horne <lawrencehorne@Gamberro.local>
2017-10-30 12:10:57 -0400

    Trouble shooting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+require('dotenv').config()
 
 require('./models/InventoryList')
 const inventoryRoutes = require('./routes/inventoryRoutes')
