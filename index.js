637fd1886e0bef07972a8e2414414b3ed9841ccf
Lawrence Horne <lawrencehorne@Gamberro.local>
2017-10-30 12:26:33 -0400

    Troubleshooting

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-require('dotenv').config()
+const path = require('path')
 
 require('./models/InventoryList')
 const inventoryRoutes = require('./routes/inventoryRoutes')
@@ -18,7 +19,6 @@ inventoryRoutes(app)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
 
-  const path = require('path')
   app.get('*', (request, response) => {
     response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
