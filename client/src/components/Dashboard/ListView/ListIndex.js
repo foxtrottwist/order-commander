c25d4d3c75cdd1505d957b460190d9e920622585
Law Horne <foxtrottwist@ymail.com>
2017-10-31 23:44:32 -0400

    Replacing some changes lost in revert

--- client/src/components/Dashboard/ListView/ListIndex.js
+++ client/src/components/Dashboard/ListView/ListIndex.js
@@ -3,12 +3,23 @@ import { InnerListBox } from './utils/sharedStyles';
 import styled from 'styled-components';
 
 const ListHeader = styled.div`
+  display: inline-block;
+  width: 13rem;
   background-color: ${({ list, selectedList }) =>
     list === selectedList ? 'rgba(0, 0, 0, 0.1)' : 'none'};
+  border-radius: 2%;
+  box-sizing: border-box;
+  margin-bottom: 1.2rem;
   border-radius: 1%;
   :hover {
     cursor: pointer;
     background-color: rgba(0, 0, 0, 0.1);
+  }
+  h4 {
+    margin-bottom: 0.65rem;
+  }
+  p + p {
+    margin-bottom: 0;
   }
 `;
 
@@ -22,6 +33,7 @@ const ListIndex = ({ lists, onSelectList, selectedList }) => (
         selectedList={selectedList}
       >
         <h4>{title}</h4>
+        <p>Created on: {new Date(dateCreated).toLocaleDateString()}</p>
         <p>Contains {list.length} items</p>
       </ListHeader>
     ))}
