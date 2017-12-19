3fedf3409daeaa9855e480758647a2da8c05e625
Law Horne <foxtrottwist@ymail.com>
2017-12-18 21:07:22 -0500

    Removed poor error handling

--- routes/createUserRoute.js
+++ routes/createUserRoute.js
@@ -6,14 +6,7 @@ const User = mongoose.model('users')
 router.route('/').post(async (req, res) => {
   try {
     const { email, password, isAdmin } = req.body
-    User.findOne({ email }).then((userExist) => {
-      if (userExist) {
-        res
-          .status(422)
-          .send({ error: 'Email already in use' })
-          .catch(error => res.send(error))
-      }
-    })
+
     const user = new User({
       email,
       password,
@@ -23,7 +16,7 @@ router.route('/').post(async (req, res) => {
     await user.save()
     res.json({ sucess: true })
   } catch (error) {
-    res.status(422).send(error.message)
+    res.send(error.message)
   }
 })
 
